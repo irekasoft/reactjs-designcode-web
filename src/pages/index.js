04a3cc8bef996e9dcd1828798573f7ee40359dd6
Muhammad Hijazi (MBP, Touch) <hijazi@irekasoft.com>
2018-07-28 14:37:14 +0800

    next commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>System of the known</h1>
+    <h1>Awesome</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Watch the Video</Link>
