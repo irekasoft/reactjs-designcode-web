fa4487b1fabdb36d6a6a5bdfd7f936ce833af6dd
Muhammad Hijazi (MBP, Touch) <hijazi@irekasoft.com>
2018-07-28 14:36:10 +0800

    hello

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>System of the known</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Watch the Video</Link>
+
+
+  </div>
+)
+
+export default IndexPage
